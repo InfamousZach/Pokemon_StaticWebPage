e4ec1d1b60b7a2cfffcc6ac9ea33d2d3d581296a
Zachary A <zachamstutz1@gmail.com>
2023-11-15 19:53:09 -0500

    Finished web page

--- slide.js
+++ slide.js
@@ -14,8 +14,7 @@ showSlides(slideIndex = n);
 function showSlides(n) {
 let i;
 let slides = document.getElementsByClassName("mySlides");
-let dots = document.getElementsByClassName("demo");
-let captionText = document.getElementById("caption");
+let dots = document.getElementsByClassName("dot");
 if (n > slides.length) {slideIndex = 1}
 if (n < 1) {slideIndex = slides.length}
 for (i = 0; i < slides.length; i++) {
@@ -26,5 +25,4 @@ for (i = 0; i < dots.length; i++) {
 }
 slides[slideIndex-1].style.display = "block";
 dots[slideIndex-1].className += " active";
-captionText.innerHTML = dots[slideIndex-1].alt;
 }
